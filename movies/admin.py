082bf5e3221d6c62efe842b2db6c425fd9e049a9
Hania93 <hannanawrot.93@gmail.com>
2026-06-11 18:42:05 +0200

    Add movie models admin and data import

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Director, Actor, Genre, Movie, MovieActor
 
-# Register your models here.
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "director", "get_genres", "release_date")
+    list_filter = ("genres", "release_date", "director")
+    search_fields = ("title", "director__name")
+    
+    def get_genres(self, obj):
+        return ", ".join(genre.name for genre in obj.genres.all())
+
+    get_genres.short_description = "Genres"
+
+
+@admin.register(MovieActor)
+class MovieActorAdmin(admin.ModelAdmin):
+    list_display = ("movie", "actor", "role_name", "billing_order")
+    list_filter = ("movie",)
--- screenings/apps.py
+++ screenings/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class ScreeningsConfig(AppConfig):
-    name = 'screenings'
+    name = "screenings"
