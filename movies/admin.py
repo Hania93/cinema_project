9ca0f2db16603229fbbe80d27604aa2a89fd259d
Hania93 <hannanawrot.93@gmail.com>
2026-06-13 00:32:20 +0200

    Add movie list and detail pages

--- movies/admin.py
+++ movies/admin.py
@@ -5,12 +5,21 @@ from .models import Director, Actor, Genre, Movie, MovieActor
 @admin.register(Director)
 class DirectorAdmin(admin.ModelAdmin):
     list_display = ("name",)
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ("tmdb_id",)
+        return ()
 
 
 @admin.register(Actor)
 class ActorAdmin(admin.ModelAdmin):
     list_display = ("name",)
-
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ("tmdb_id",)
+        return ()
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
@@ -22,6 +31,11 @@ class MovieAdmin(admin.ModelAdmin):
     list_display = ("title", "director", "get_genres", "release_date")
     list_filter = ("genres", "release_date", "director")
     search_fields = ("title", "director__name")
+    
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ("tmdb_id",)
+        return ()
     
     def get_genres(self, obj):
         return ", ".join(genre.name for genre in obj.genres.all())
--- movies/views.py
+++ movies/views.py
@@ -1,1 +1,60 @@
-# Create your views here.
+from django.views.generic import DetailView, ListView
+
+from .models import Genre, Movie
+
+
+class MovieListView(ListView):
+    model = Movie
+    template_name = "movies/movie_list.html"
+    context_object_name = "movies"
+    paginate_by = 10
+
+    def get_queryset(self):
+        qs = Movie.objects.select_related("director").prefetch_related(
+            "genres", "actors"
+        )
+
+        genre_id = self.request.GET.get("genre")
+
+        if genre_id:
+            qs = qs.filter(genres__id=genre_id)
+
+        q = self.request.GET.get("q")
+
+        if q:
+            qs = qs.filter(title__icontains=q)
+
+        return qs.distinct()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["genres"] = Genre.objects.all()
+
+        context["current_q"] = self.request.GET.get("q", "")
+        
+        context["query_params"] = self.request.GET.copy()
+        context["query_params"].pop("page", None)
+
+        return context
+
+
+class MovieDetailView(DetailView):
+    model = Movie
+    template_name = "movies/movie_detail.html"
+    context_object_name = "movie"
+
+    def get_queryset(self):
+
+        return Movie.objects.select_related("director").prefetch_related(
+            "genres", "movieactor_set__actor"
+        )
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cast"] = (
+            self.object.movieactor_set
+            .select_related("actor")
+            .order_by("billing_order")
+        )
+        return context   
