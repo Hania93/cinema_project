962638481351d96399416286c9bde3ce0fee10e1
Hania93 <hannanawrot.93@gmail.com>
2026-06-14 22:17:56 +0200

    Add user authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
+from .views import RegisterView
 
 urlpatterns = [
     path(
@@ -12,4 +13,9 @@ urlpatterns = [
          LogoutView.as_view(),
          name="logout",
     ),
+    path(
+        "register/",
+        RegisterView.as_view(),
+        name="register"
+    )
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from .forms import RegisterForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("login")
