bbd834420b0656e182c1bb24b55dde58c03cfde0
Hania93 <hannanawrot.93@gmail.com>
2026-06-18 00:45:02 +0200

    complete tests creating

---
+++ reservations/emails.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+def send_reservation_confirm_email(reservation):
+    user = reservation.user
+    screening = reservation.screening
+    
+    if not user.email:
+        return
+    
+    seats = reservation.reserved_seats.select_related("seat").all()
+    
+    seats_text = ", ".join(
+        f"Rząd {reservation_seat.seat.row}, miejsce {reservation_seat.seat.number}"
+        for reservation_seat in seats
+    )
+    
+    subject = "Potwierdzenie rezerwacji"
+    
+    message = (
+        f"Cześć {user.username}, \n\n"
+        f"Twoja rezerwacja została utowrzona. \n\n"
+        f"Film: {screening.movie.title}\n"
+         f"Data: {screening.start_time:%d.%m.%Y}\n"
+        f"Godzina: {screening.start_time:%H:%M}\n"
+        f"Sala: {screening.hall.name}\n"
+        f"Miejsca: {seats_text}\n"
+        f"Koszt: {reservation.total_cost} zł\n\n"
+        f"Dziękujemy za rezerwację!"
+    )
+    
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user.email],
+        fail_silently=False
+    )
--- screenings/tests.py
+++ screenings/tests.py
@@ -1,1 +1,42 @@
-# Create your tests here.
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+from django.utils import timezone
+
+from movies.models import Director, Movie
+from screenings.models import Hall, Screening
+
+class ScreeningModelTests(TestCase):
+    def setUp(self):
+        self.director = Director.objects.create(
+            name="Test Director",
+        )
+
+        self.movie = Movie.objects.create(
+            title="Test Movie",
+            director=self.director,
+            duration=120,
+        )
+
+        self.hall = Hall.objects.create(
+            name="Sala 1",
+            rows=5,
+            seats_per_row=10,
+        )
+
+    def test_screening_cannot_overlap_in_same_hall(self):
+        start_time = timezone.now() + timezone.timedelta(days=1)
+
+        Screening.objects.create(
+            movie=self.movie,
+            hall=self.hall,
+            start_time=start_time,
+        )
+
+        overlapping_screening = Screening(
+            movie=self.movie,
+            hall=self.hall,
+            start_time=start_time + timezone.timedelta(minutes=30),
+        )
+
+        with self.assertRaises(ValidationError):
+            overlapping_screening.full_clean()
